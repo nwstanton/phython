92584493bb05737fadb2266bbc2a751bdc31fe90
Natnan Stanton <66800414+nwstanton@users.noreply.github.com>
2020-09-18 15:29:38 -0500

    Add files via upload

--- phython/equations/phyEquat.py
+++ phython/equations/phyEquat.py
@@ -1,1 +1,22 @@
-iuiuqgfiu
+import math
+
+def calcAcc(projectile, enviroment):
+    if env.isFrFall:
+        proj1.acc = env.grav
+    else:
+        proj1.acc = proj1.force / proj1.mass
+
+def calcKE(arg):
+    pass
+
+def calcPE(arg):
+    pass
+
+def calcBounceHt(arg):
+    bounceHt = proj1.pos * env.restCoef
+
+def calcDrop(arg):
+    pass
+
+def calcTimeOfImpact(arg):
+    timeOfImpact = math.sqrt((2*proj1.pos)/proj1.acc)
