53494ba1a87aadf3bf7ef782f17c9e3fc570e265
Natnan Stanton <66800414+nwstanton@users.noreply.github.com>
2020-09-18 15:30:32 -0500

    Add files via upload

--- phython/physEng.py
+++ phython/physEng.py
@@ -1,8 +1,9 @@
-import math
+
 import phyUI.py
+import phyEquat.py
 #parent classes
 class projectile:
-    def __init__(self, mass, mat, airRes, force, mome, pos, pe, ke):
+    def __init__(self, mass, mat, airRes, force, mome, pos, pe, ke, acc, v):
         self.mass = mass #in KG
         self.mat = mat
         self.airRes = airRes
@@ -11,13 +12,16 @@ class projectile:
         self.pos = pos # height in meters
         self.pe = pe
         self.ke = ke
+        self.acc = acc
+        self.v = v
 
 class enviroment:
-    def __init__(self, grav, surfOnsurf, time, restCoef):
+    def __init__(self, grav, surfOnsurf, time, restCoef, isFrFall):
         self.grav = grav
         self.surfOnsurf = surfOnsurf
         self.time = time
         self.restCoef = restCoef
+        self.isFrFall = isFrFall
 
 #list of materials
 materials = ["wood", "steel"]
@@ -34,7 +38,7 @@ resttitution = {
     "SteelOnSteel" : .56
 }
 #creating initial children
-proj1 = projectile(1, materials[0], 0, 0, 0, 1, 0, 0)
-proj2 = projectile(5000000, materials[0], 0, 0, 0, 0, 0, 0)
-env = enviroment(9.8, 0, 0, 0)
+proj1 = projectile(1, materials[0], 0, 0, 0, 1, 0, 0, 0, 0)
+proj2 = projectile(5000000, materials[0], 0, 0, 0, 0, 0, 0, 0, 0)
+env = enviroment(9.8, 0, 0, 0, True)
 #initalize all children as zero and wait to get selections from user
