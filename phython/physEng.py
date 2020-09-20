893288141aaeea92735c372c1ec086311fbba662
Natnan Stanton <66800414+nwstanton@users.noreply.github.com>
2020-09-20 13:09:18 -0500

    Add files via upload

--- phython/physEng.py
+++ phython/physEng.py
@@ -1,6 +1,6 @@
 
-import phyUI.py
-import phyEquat.py
+from phyUI import *
+from phyEquat import *
 #parent classes
 class projectile:
     def __init__(self, mass, mat, airRes, force, mome, pos, pe, ke, acc, v):
@@ -16,22 +16,28 @@ class projectile:
         self.v = v
 
 class enviroment:
-    def __init__(self, grav, surfOnsurf, time, restCoef, isFrFall):
+    def __init__(self, grav, surfOnsurf, time, restCoef, isFrFall, sysEng):
         self.grav = grav
         self.surfOnsurf = surfOnsurf
         self.time = time
         self.restCoef = restCoef
         self.isFrFall = isFrFall
+        self.sysEng = sysEng
+
+class surface:
+    def __init__(self, mat, pos):
+        self.mat = mat
+        self.pos = pos
 
 #list of materials
 materials = ["wood", "steel"]
-#dict of surfOnsurf
+#dict of surf to surface friction coeficients
 surfFriction = {
     "WoodOnWood" : .2,
     "WoodOnSteel" : .35,
     "SteelOnSteel" : .57
 }
-#dict of restCoef
+#dict of resittitution coeficients
 resttitution = {
     "WoodOnWood" : .5,
     "WoodOnSteel" :.53,
@@ -39,6 +45,6 @@ resttitution = {
 }
 #creating initial children
 proj1 = projectile(1, materials[0], 0, 0, 0, 1, 0, 0, 0, 0)
-proj2 = projectile(5000000, materials[0], 0, 0, 0, 0, 0, 0, 0, 0)
-env = enviroment(9.8, 0, 0, 0, True)
+surf = surface(materials[0], 0)
+env = enviroment(9.8, 0, 0, 0, True, 0)
 #initalize all children as zero and wait to get selections from user
