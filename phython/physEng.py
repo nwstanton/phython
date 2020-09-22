1ad6b5f0db25da0271f348594a5f82a8adb91329
Natnan Stanton <66800414+nwstanton@users.noreply.github.com>
2020-09-21 23:44:55 -0500

    Add files via upload

--- phython/physEng.py
+++ phython/physEng.py
@@ -1,6 +1,8 @@
-
 from phyUI import *
 from phyEquat import *
+import pygame
+import time
+
 #parent classes
 class projectile:
     def __init__(self, mass, mat, airRes, force, mome, pos, pe, ke, acc, v):
@@ -43,8 +45,34 @@ resttitution = {
     "WoodOnSteel" :.53,
     "SteelOnSteel" : .56
 }
+#dict of gravity settings
+gravSet = {
+    "earth" : 9.801,
+    "mars" : 3.711,
+    "jupiter" : 24.790
+}
+
 #creating initial children
 proj1 = projectile(1, materials[0], 0, 0, 0, 1, 0, 0, 0, 0)
 surf = surface(materials[0], 0)
-env = enviroment(9.8, 0, 0, 0, True, 0)
+env = enviroment(9.801, 0, 0, 0, True, 0)
+
+#dict of projectile data at time slices
+#seconds : projectile data
+projData = {
+    0 : proj1
+}
+
+#dict of enviromet data at time slices
+#seconds : env data
+envData = {
+    0 : env
+}
+
+
 #initalize all children as zero and wait to get selections from user
+
+#after all user inputs have been taken and applied to the objects
+#runSim() function should be called on button click of the "play" button
+def runSim(proj, env, surf):
+    pass
